1815d9e1577c89edca7421f7104bd75fca2102d4
hyunbin <jshyunbin@gmail.com>
2022-10-06 03:25:17 -0400

    :heavy_plus_sign: Adding a dependency.
    Using winit and pixels crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
